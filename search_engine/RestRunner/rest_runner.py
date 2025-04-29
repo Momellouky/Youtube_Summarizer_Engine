0dbd475ce299794e99cd315290090d1e38560d7d
moMellouky <contact.mellouky@gmail.com>
2025-04-29 17:35:15 +0200

    doc: add top-doc of the rest_runner class file

---
+++ search_engine/RestRunner/rest_runner.py
@@ -0,0 +1,16 @@
+"""rest_runner.py
+Author: Mohamed MELLOUKY
+Date: 29/04/2025
+Description:
+This module provides a class to run REST API tests using the flask framework.
+It allows you to run the endpoints and get the response.
+
+Example usage:
+    from RestRunner.rest_runner import RestRunner
+
+    runner = RestRunner()
+    runner.run()
+
+    # To stop the server, use:
+    runner.stop()
+"""
