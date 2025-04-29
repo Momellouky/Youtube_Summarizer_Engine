4ad0376b85a205600ab057a7a22d304d9d254b43
moMellouky <contact.mellouky@gmail.com>
2025-04-29 17:32:27 +0200

    doc: document the __init__ file of the RestRunner package

---
+++ search_engine/RestRunner/__init__.py
@@ -0,0 +1,4 @@
+"""RestRunner: A Python package for running REST API tests.
+This package provides and interface for running the REST API endpoints.
+
+It allows you to run the endpoints and get the response."""
