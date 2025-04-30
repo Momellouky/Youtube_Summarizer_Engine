406fc6e15c1ee6c5177c89e09c788fc047149d76
moMellouky <contact.mellouky@gmail.com>
2025-04-30 13:42:26 +0200

    feat: start the server

---
+++ search_engine/main.py
@@ -0,0 +1,10 @@
+from RestRunner.rest_runner import RestRunner
+
+
+
+if __name__ == "__main__":
+    # Create an instance of the RestRunner class
+    runner = RestRunner()
+
+    # Run the Flask app
+    runner.run()
